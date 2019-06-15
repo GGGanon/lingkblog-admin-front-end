1e9d8ae378e1e4b366b64f70507e69868fbfc5e2
chenshaoming <csming1995@gmail.com>
2019-06-15 22:45:51 +0800

    [Feature] Add Article List.

---
+++ src/views/article/ArticleList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class ArticleList extends React.Component {
+
+    render () {
+        return (
+            <div>
+                Articles
+            </div>
+        )
+    }
+}
+
+export default ArticleList;
